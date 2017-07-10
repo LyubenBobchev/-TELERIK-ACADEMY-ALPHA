3165eecdfe46d7f962da974deb13e9bbe9de4d0a
LyubenBobchev <lsbworkedu@gmail.com>
2017-07-10 19:38:09 +0300

    update 10.07.17

---
+++ CsharpAdvanced/Arrays/PrimeNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.Parse(Console.ReadLine());
+
+            bool asd = true;
+
+            Console.WriteLine(asd);
+
+        }
+    }
+}
