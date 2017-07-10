54038acf37c0f6809eece73a4036cb955dc2ed9a
Lyuben Bobchev <lsbworkedu@gmail.com>
2017-07-10 19:36:11 +0300

    update 10.07.17

---
+++ CsharpAdvanced/IOWorkingWithFiles/OddLines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../test.txt";
+            var reader = new StreamReader(filePath);
+
+            using (reader)
+            {
+                int lineNumber = 1;
+                string line = "";
+
+                while (line != null)
+                {
+
+                    line = reader.ReadLine();
+                    if (lineNumber % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineNumber++;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ CsharpAdvanced/Strings/TEST/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+
+            string toUpper = inputText.ToUpper();
+
+            Console.WriteLine(toUpper);
+        }
+    }
+}
